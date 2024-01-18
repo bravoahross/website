fb1228631401655b92218ee8696ffc7aa40c82a0
ncross3392 <ncross4@iastate.edu>
2024-01-17 19:37:25 -0600

    broke link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.orgfdsdv"
           target="_blank"
           rel="noopener noreferrer"
         >
